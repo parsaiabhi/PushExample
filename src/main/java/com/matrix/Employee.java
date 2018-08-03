b5556b53a6b250d1d8ef6099f8d17cb04524d9c5
abhishek <tech.parsai@gmail.com>
2018-08-03 21:55:24 +0530

    employee file added

---
+++ src/main/java/com/matrix/Employee.java
@@ -0,0 +1,17 @@
+package com.matrix;
+
+public class Employee {
+	
+	private int empId;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	
+	
+
+}
