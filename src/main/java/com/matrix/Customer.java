fff7450f648d6efcb75e90ae4385b01f36a01e8c
abhishek <tech.parsai@gmail.com>
2018-08-03 21:56:56 +0530

    customer file added

---
+++ src/main/java/com/matrix/Customer.java
@@ -0,0 +1,16 @@
+package com.matrix;
+
+public class Customer {
+	private long custId;
+
+	public long getCustId() {
+		return custId;
+	}
+
+	public void setCustId(long custId) {
+		this.custId = custId;
+	}
+	
+	
+
+}
