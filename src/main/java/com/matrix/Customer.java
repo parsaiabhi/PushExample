f8002039cbff0fa3d363989839953533316e7c11
parsaiabhi <37465432+parsaiabhi@users.noreply.github.com>
2018-08-04 07:18:33 +0530

    Update Customer.java

--- src/main/java/com/matrix/Customer.java
+++ src/main/java/com/matrix/Customer.java
@@ -1,14 +1,25 @@
 package com.matrix;
 
 public class Customer {
+	
+	private List<Employee> empList;
+	public Customer(){
+	
+		empList = new ArrayList<Employee>();
+	}
 	private long custId;
 
+	
 	public long getCustId() {
 		return custId;
 	}
 
 	public void setCustId(long custId) {
 		this.custId = custId;
+	}
+	
+	public void addEmployee(Employee emp){
+		empList.add(emp);
 	}
 	
 	
